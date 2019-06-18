b992abdc44860cdf0495a1c68a2fa6601be9337f
Mike Combs <mike@combsnet.com>
2019-06-18 14:57:26 -0400

    feat: moved flask key out of source code

--- rpgtools/__init__.py
+++ rpgtools/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '0ad77153aad20ae837e53440ad7d9dd6'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt()
