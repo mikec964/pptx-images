e8cb1c9f4cd82cf2a22fcd27251fe57282a30c29
Mike Combs <mike@combsnet.com>
2019-06-18 17:10:33 -0400

    feat: login, logout

--- rpgtools/__init__.py
+++ rpgtools/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 import os
 
 
@@ -9,5 +10,6 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt()
+login_manager = LoginManager(app)
 
 from rpgtools import routes
--- rpgtools/routes.py
+++ rpgtools/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template, url_for, flash, redirect
+from flask_login import login_user, current_user, logout_user
 from rpgtools import app, db, bcrypt
 from rpgtools.forms import RegistrationForm, LoginForm
 from rpgtools.models import User, Post
@@ -17,6 +18,8 @@ def about():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_pw = bcrypt.generate_password_hash(form.password.data).decode('utf-8)')
@@ -30,11 +33,21 @@ def register():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@example.com' and form.password.data == 'password':
-            flash('You have logged in.', 'success')
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            # flash('You have logged in.', 'success')
             return redirect(url_for('home'))
         else:
-            flash('Login unsuccessful. Please check username and password.', 'danger')
+            flash('Login unsuccessful. Please check email and password.', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
