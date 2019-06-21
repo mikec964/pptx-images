114a2c809074e08ee97f1d53a19b56d956798c95
Mike Combs <mike@combsnet.com>
2019-06-20 22:43:02 -0400

    feat: update blog posts

--- rpgtools/routes.py
+++ rpgtools/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from flask_login import login_user, current_user, logout_user, login_required
 from PIL import Image
 import os
@@ -101,5 +101,34 @@ def new_post():
         db.session.commit()
         flash('Your post has been accepted.', 'success')
         return redirect(url_for('home'))
-    return render_template('new_post.html', title='New Post', form=form)
+    return render_template('new_post.html', title='New Post',
+                            legend='New Post', form=form)
+
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        # we don't need to create a new post object, or add to db
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated.', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content 
+    return render_template('new_post.html', title='Update Post', 
+        legend='Update Post', form=form)
+
 
