e19be8b8b971c718d1172dd0e5b6a1e001778298
Mike Combs <mike@combsnet.com>
2019-06-24 23:42:59 -0400

    feat: changed private config keys from .env to .yaml

--- rpgtools/config.py
+++ rpgtools/config.py
@@ -1,11 +1,12 @@
-import os
-
+import ruamel_yaml as yaml
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+    with open("keys.yaml", 'r') as config_f:
+        keys = yaml.safe_load(config_f)
+    SECRET_KEY = keys['SECRET_KEY']
+    SQLALCHEMY_DATABASE_URI = keys['DATABASE_URI']
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    MAIL_USERNAME = keys['EMAIL_USER']
+    MAIL_PASSWORD = keys['EMAIL_PASS']
